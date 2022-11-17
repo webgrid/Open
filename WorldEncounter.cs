22153a997865139468d47bbedefc27d8beef6ce4
WebGrid <olav@webgrid.com>
2022-11-17 09:48:30 +0100

    C# and Typescript examples
    WorldEncounter.cs is a specialized class for handling game encounters (spawning objects or beings).
    feedback.ts is a stand-alone class that dynamically loads when requested by an user.

---
+++ WorldEncounter.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WorldObjects.Tools;
+using WorldObjects.Tools.Extensions;
+using WorldObjects.World.Enums;
+using WorldObjects.World.Interfaces;
+using WorldObjects.World.Living;
+using WorldObjects.World.Living.Player;
+using WorldObjects.World.MessageNoise;
+
+namespace WorldObjects.World.Map
+{
+    /// <summary>
+    /// World encounter are triggered when objects enters, moves, or leaves a collection of blocks.
+    /// </summary>
+    public sealed class WorldEncounter : WorldEffectArea, IHeartbeat
+    {
+        public WorldEncounter(World world, WorldBlock origin, byte radius) : base(world, origin, radius)
+        {
+            ///Default values
+            TriggerChance = new Tuple<byte, DiceNotation>(10, new DiceNotation("4d6"));
+            TriggerRetry = DateTime.MinValue;
+            Encounters = new();
+            Players = new ();
+            Leave = new List<Func<IWorldInfo, ResultInfo>>{ (IWorldInfo obj) =>
+            {
+                if (obj.GameObjectType == GameObjectType.Living && obj is PlayerInfo player)
+                {
+                    Players.Remove(player);
+                }
+                return ResultInfo.ReturnWithOk();
+            } };
+
+            Enter = new List<Func<IWorldInfo, ResultInfo>>{(IWorldInfo obj) =>
+            {
+                if (obj.GameObjectType == GameObjectType.Living && obj is PlayerInfo player)
+                {
+                    Players.Add(player);
+                }
+                return ResultInfo.ReturnWithOk();
+            } };
+            Game.Configuration.AddObject(this);
+        }
+
+        public WorldEncounterSpawn ActiveEncounter { get; private set; }
+
+        /// <summary>
+        /// Min and Max players within encounter area
+        /// </summary>
+        public Tuple<byte, byte> ConditionMinMaxPlayers { get; set; }
+
+        public List<WorldEncounterSpawn> Encounters { get; }
+
+        public override GameObjectType GameObjectType => GameObjectType.WorldEncounter;
+
+        public HashSet<PlayerInfo> Players { get; }
+
+        /// <summary>
+        /// Trigger seconds interval, chance %
+        /// </summary>
+        public Tuple<byte, DiceNotation> TriggerChance { get; set; }
+
+        public DateTime TriggerRetry { get; set; }
+
+        public WorldEncounterSpawn GetRandomEncounter()
+        {
+            var total = Encounters.Sum(t => t.Chance);
+
+            var successnumber = Utility.RandomNumber(0, total);
+            var counter = 0;
+            foreach (var item in Encounters)
+            {
+                if (successnumber.Between(counter, counter + item.Chance))
+                    return item;
+                counter += item.Chance;
+            }
+            return null;
+        }
+
+        public void Heartbeat()
+        {
+            if (ActiveEncounter != null)
+            {
+                if (Utility.DataBaseNow > ActiveEncounter.InactivityTimeout)
+                {
+                    if (ActiveEncounter.SpawnedObjects != null)
+                        ActiveEncounter.EndEncounter();
+                    else if (Utility.DataBaseNow > ActiveEncounter.CooldownExpires)
+                        ActiveEncounter = null;
+                }
+                else if (ActiveEncounter.SpawnedObjects?.Any(t => t.GameObjectType == GameObjectType.Living && ((LivingInfo)t).WeaponHitYou.Count > 0) == true)
+                    ActiveEncounter.InactivityTimeout = Utility.DataBaseNow.AddSeconds(ActiveEncounter.DurationSeconds);
+            }
+
+            if (ActiveEncounter != null || Players.Count == 0 || TriggerRetry > Utility.DataBaseNow)
+                return;
+
+            TriggerRetry = Utility.DataBaseNow.AddSeconds(TriggerChance.Item1);
+
+            if (ConditionMinMaxPlayers != null && !Players.Count.Between(ConditionMinMaxPlayers.Item1, ConditionMinMaxPlayers.Item2))
+                return;
+
+            if (TriggerChance.Item2.Roll() > Utility.RandomHundred())
+            {
+                ActiveEncounter = GetRandomEncounter();
+                ActiveEncounter?.Init(this);
+            }
+        }
+    }
+
+    public class WorldEncounterSpawn
+    {
+        public WorldEncounterSpawn()
+        {
+            CooldownExpires = DateTime.MinValue;
+            CooldownSeconds = 120;
+            DurationSeconds = 60;
+        }
+
+        public byte Chance { get; set; }
+
+        public ushort CooldownSeconds { get; set; }
+
+        public ushort DurationSeconds { get; set; }
+
+        public Func<List<IWorldInfo>> Encounter { get; set; }
+
+        public List<IWorldInfo> SpawnedObjects { get; private set; }
+
+        public MessageInfo Start { get; set; }
+
+        internal DateTime CooldownExpires { get; set; }
+
+        internal DateTime InactivityTimeout { get; set; }
+
+        public void Init(WorldEncounter encounter)
+        {
+            SpawnedObjects = Encounter();
+            InactivityTimeout = Utility.DataBaseNow.AddSeconds(DurationSeconds);
+            foreach (var obj in SpawnedObjects)
+            {
+                var cell = encounter.EffectedCells.GetRandomAvailable(obj, encounter.World);
+                if (cell != null)
+                    obj.SetWorld(encounter.World, cell);
+            }
+        }
+
+        internal void EndEncounter()
+        {
+            for (int i = 0; i < SpawnedObjects.Count; i++)
+            {
+                var obj = SpawnedObjects[i];
+
+                if (obj.InventoryOwner == null)
+                {
+                    if (obj.GameObjectType == GameObjectType.Living && obj is ILiving living)
+                    {
+                        if (!living.IsMeleeAttack && living.Condition == LivingConditionType.Conscious)
+                        {
+                            Game.Configuration.RemoveObject(obj);
+                            SpawnedObjects.Remove(obj);
+                        }
+                    }
+                    else
+                    {
+                        Game.Configuration.RemoveObject(obj);
+                        SpawnedObjects.Remove(obj);
+                    }
+                }
+            }
+            SpawnedObjects = null;
+            CooldownExpires = Utility.DataBaseNow.AddSeconds(CooldownSeconds);
+        }
+    }
+}
---
+++ feedback.ts
@@ -0,0 +1,217 @@
+﻿/*
+ * Player feedback which is initilized from the UI by link. Player provides feedback-category and short description, and optional a screenshot from drawn by a rectangle.
+ */
+class mgc_PlayerFeedback {
+    mgp_isSelecting: boolean;
+    mgp_isInitilized: boolean;
+    mgp_start: any;
+    mgp_end: any;
+    mgp_feedback_message: string;
+    mgp_feedback_imagepngdata: string;
+    mgp_feedback_type: string;
+    constructor() {
+        // Things we need to keep track of
+        this.mgp_start = {};
+        this.mgp_end = {};
+        this.mgp_isSelecting = false;
+        this.mgp_isInitilized = false;
+    }
+
+    mgp_init(callback: Function) {
+        if (this.mgp_isInitilized)
+            return;
+        if (typeof html2canvas == "undefined")
+            $.getScript("/lib/html2canvas.min.js").done((script, textStatus) => { callback(); });
+        else
+            callback();
+        this.mgp_isInitilized = true;
+    }
+    capture() {
+        if (!this.mgp_isInitilized) {
+            this.mgp_init(() => { this.capture(); });
+            return;
+        }
+
+        this.mgp_start = {};
+        this.mgp_end = {};
+        this.mgp_feedback_message = $("#mg_feedback_message").val() as string;
+        this.mgp_feedback_type = $("#mg_feedback_type").val() as string;
+        Core.World.map.mgp_modalpanel.close();
+        Core.World.player.mgp_playerlog.mgm_addnotification("Click and move mousepointer", "Capture a screenshot region");
+        this.mgm_bind();
+    }
+
+    submit() {
+        this.mgp_feedback_message = $("#mg_feedback_message").val() as string;
+        this.mgp_feedback_type = $("#mg_feedback_type").val() as string;
+        if (this.mgp_feedback_message == null || this.mgp_feedback_message.length < 1)
+            return;
+
+        if (this.mgp_feedback_imagepngdata == null)
+            this.mgp_feedback_imagepngdata = "";
+        Core.mgp_ServerCom.mgm_messageCallback(null, mge_SocketMessageType.PlayerMenu, { action: "feedback_submit", feedback_type: this.mgp_feedback_type, feedback_message: this.mgp_feedback_message, feedback_imagedata: this.mgp_feedback_imagepngdata }, (caller: any, result) => {
+            if (Core.World.map.mgp_modalpanel != null)
+                Core.World.map.mgp_modalpanel.close();
+            this.mgp_feedback_imagepngdata = null;
+            this.mgp_feedback_message = null;
+            $("#mg_feedback_message").val("");
+            Core.World.player.mgp_playerlog.mgm_addnotification("Thank you! Your message has successfully been submitted.", "Feedback");
+        });
+    }
+
+    screenshot(element, options) {
+        // our cropping context
+
+        // save the passed width and height
+        const finalWidth = options.width || window.innerWidth;
+        const finalHeight = options.height || window.innerHeight;
+
+        if (options.x)
+            options.width = finalWidth + options.x;
+        if (options.y)
+            options.height = finalHeight + options.y;
+
+        /*This is important to make it crop correctly*/
+        options.scrollX = options.x;
+        options.scrollY = options.y;
+        options.windowWidth = window.outerWidth * 2;
+        options.windowHeight = window.outerHeight * 2;
+
+        return html2canvas(element, options).then(c => {
+            const cropcanvas = document.createElement("canvas").getContext("2d");
+            cropcanvas.canvas.width = finalWidth;
+            cropcanvas.canvas.height = finalHeight;
+            cropcanvas.drawImage(c, options.x, options.y, options.width, options.height, 0, 0, options.width, options.height);
+            // return our canvas
+            return cropcanvas.canvas;
+        });
+    }
+
+    editscrn() {
+        const canvas = document.getElementById("scrnshtcanvas") as HTMLCanvasElement;
+        const ctx = canvas.getContext("2d");
+
+        //Variables
+        const canvasx = $(canvas).offset().left;
+        const canvasy = $(canvas).offset().top;
+        let last_mousex = 0;
+        let last_mousey = 0;
+        let mousex = 0;
+        let mousey = 0;
+        let mousedown = false;
+
+        //Mousedown
+        $(canvas).on("mousedown", e => {
+            last_mousex = mousex = e.clientX - canvasx;
+            last_mousey = mousey = e.clientY - canvasy;
+            mousedown = true;
+        });
+
+        //Mouseup
+        $(canvas).on("mouseup", e => {
+            mousedown = false;
+        });
+
+        //Mousemove
+        $(canvas).on("mousemove", e => {
+            mousex = e.clientX - canvasx;
+            mousey = e.clientY - canvasy;
+            if (mousedown) {
+                ctx.beginPath();
+
+                ctx.globalCompositeOperation = "source-over";
+                ctx.strokeStyle = "red";
+                ctx.lineWidth = 3;
+
+                ctx.moveTo(last_mousex, last_mousey);
+                ctx.lineTo(mousex, mousey);
+                ctx.lineJoin = ctx.lineCap = "round";
+                ctx.stroke();
+            }
+            last_mousex = mousex;
+            last_mousey = mousey;
+            //Output
+        });
+    }
+
+    savescrn() {
+        const canvas = document.getElementById("scrnshtcanvas") as HTMLCanvasElement;
+
+        const FileUrl = canvas.toDataURL("image/png");
+
+        const a = document.createElement("a");
+        a.href = FileUrl.replace("image/png", "image/octet-stream");
+        a.download = "SCREENSHOT_.png";
+        a.click();
+
+        //	$("#screenshtdialog").dialog('close');
+    }
+
+    mgm_bind() {
+        $(window)
+            // Listen for selection
+            .on("mousedown", e => {
+                // Update our state
+                this.mgp_isSelecting = true;
+                $("#mg_feedback_selection").removeClass("complete");
+                this.mgp_start.x = e.pageX;
+                this.mgp_start.y = e.pageY;
+                // Add selection to screen
+                $("#mg_feedback_selection").css({ left: this.mgp_start.x, top: this.mgp_start.y });
+            })
+            // Listen for movement
+            .on("mousemove", e => {
+                // Ignore if we're not selecing
+                if (!this.mgp_isSelecting)
+                    return;
+
+                // Update our state
+                this.mgp_end.x = e.pageX;
+                this.mgp_end.y = e.pageY;
+
+                // Move & resize selection to reflect mouse position
+                $("#mg_feedback_selection").css({
+                    left: this.mgp_start.x < this.mgp_end.x ? this.mgp_start.x : this.mgp_end.x,
+                    top: this.mgp_start.y < this.mgp_end.y ? this.mgp_start.y : this.mgp_end.y,
+                    width: Math.abs(this.mgp_start.x - this.mgp_end.x),
+                    height: Math.abs(this.mgp_start.y - this.mgp_end.y)
+                });
+            })
+            // listen for end
+            .on("mouseup", $event => {
+                // Update our state
+                this.mgp_isSelecting = false;
+                $("#mg_feedback_selection").addClass("complete");
+                $(window).unbind();
+                Core.World.player.feedback_dialog();
+
+                if (isNaN(this.mgp_start.y) || isNaN(this.mgp_start.x)) {
+                    Core.World.player.mgp_playerlog.mgm_addchanneltext("Invalid options for screenshot.", mge_PlayerChannel.SystemError);
+                    return;
+                }
+                this.screenshot(document.body, {
+                    x: this.mgp_start.x < this.mgp_end.x ? this.mgp_start.x : this.mgp_end.x,
+                    y: this.mgp_start.y < this.mgp_end.y ? this.mgp_start.y : this.mgp_end.y,
+                    width: Math.abs(this.mgp_start.x - this.mgp_end.x),
+                    height: Math.abs(this.mgp_start.y - this.mgp_end.y),
+                    useCORS: true,
+                    foreignObjectRendering: true,
+                }).then(c => {
+                    if (c == null)
+                        return;
+                    $("#mg_feedback_message").val(this.mgp_feedback_message);
+                    $("#mg_feedback_type").val(this.mgp_feedback_type);
+                    this.mgp_feedback_imagepngdata = c.toDataURL("image/jpeg", 0.75);
+
+                    if (this.mgp_feedback_imagepngdata.length > 80000) {
+                        Core.World.player.mgp_playerlog.mgm_addchanneltext(`Image is too large ${this.mgp_feedback_imagepngdata.length} (max is 80k) select a smaller region.`, mge_PlayerChannel.SystemError);
+                        this.mgp_feedback_imagepngdata = null;
+                    }
+                    else {
+                        const imgobj = $("#mg_feedbackimage")[0] as HTMLImageElement;
+                        imgobj.src = this.mgp_feedback_imagepngdata;
+                    }
+                });
+            });
+    }
+}
